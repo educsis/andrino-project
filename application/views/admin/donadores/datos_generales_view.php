590153a07f45d326381c1fa16bc974f3c86c4fb8
Edwin Orellana <educsis@gmail.com>
2023-12-03 03:53:07 -0600

    more updates

--- application/views/admin/donadores/datos_generales_view.php
+++ application/views/admin/donadores/datos_generales_view.php
@@ -4,144 +4,142 @@
             <h3 class="card-title">Datos Generales</h3>
         </div>
         <div class="card-body">
-            <form action="">
-                <div class="row mb-4">
-                    <div class="col-3">
-                        <label class="form-label">Tipo de donación</label>
-                        <div>
-                            <label class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="tipo_donacion" checked>
-                                <span class="form-check-label">Sangre Completa</span>
-                            </label>
-                            <label class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="tipo_donacion" >
-                                <span class="form-check-label">Aferesis</span>
-                            </label>
-                        </div>
-                    </div>
-                    <div class="col-3">
-                        <label class="form-label">DPI / Pasaporte</label>
-                        <input type="text" class="form-control">
-                    </div>
-                    <div class="col-6">
-                        <label class="form-label">Nombres y Apellidos</label>
-                        <input type="text" class="form-control">
+            <div class="row mb-4">
+                <div class="col-3">
+                    <label class="form-label">Tipo de donación</label>
+                    <div>
+                        <label class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="tipo_donacion" checked>
+                            <span class="form-check-label">Sangre Completa</span>
+                        </label>
+                        <label class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="tipo_donacion" >
+                            <span class="form-check-label">Aferesis</span>
+                        </label>
                     </div>
                 </div>
-                <div class="row mb-4">
-                    <div class="col-3">
-                        <label class="form-label">Género</label>
-                        <div>
-                            <label class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="genero" checked>
-                                <span class="form-check-label">Masculino</span>
-                            </label>
-                            <label class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="genero" >
-                                <span class="form-check-label">Femenino</span>
-                            </label>
-                        </div>
-                    </div>
-                    <div class="col-9">
-                        <label class="form-label">Grupo Étnico</label>
-                        <div>
-                            <label class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="etnico" checked>
-                                <span class="form-check-label">Mestizo</span>
-                            </label>
-                            <label class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="etnico" >
-                                <span class="form-check-label">Maya</span>
-                            </label>
-                            <label class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="etnico" >
-                                <span class="form-check-label">Garifuna</span>
-                            </label>
-                            <label class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="etnico" >
-                                <span class="form-check-label">Xinca</span>
-                            </label>
-                            <label class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="etnico" >
-                                <span class="form-check-label">Otros / Extranjeros</span>
-                            </label>
-                            <label class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="etnico" >
-                                <span class="form-check-label">No Indica</span>
-                            </label>
-                        </div>
+                <div class="col-3">
+                    <label class="form-label">DPI / Pasaporte</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-6">
+                    <label class="form-label">Nombres y Apellidos</label>
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+            <div class="row mb-4">
+                <div class="col-3">
+                    <label class="form-label">Género</label>
+                    <div>
+                        <label class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="genero" checked>
+                            <span class="form-check-label">Masculino</span>
+                        </label>
+                        <label class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="genero" >
+                            <span class="form-check-label">Femenino</span>
+                        </label>
                     </div>
                 </div>
-                <div class="row mb-4">
-                    <div class="col-4">
-                        <label class="form-label">Profesión u Oficio</label>
-                        <input type="text" class="form-control">
-                    </div>
-                    <div class="col-3">
-                        <label class="form-label">Comunidad Lingüistica</label>
-                        <input type="text" class="form-control">
-                    </div>
-                    <div class="col-2">
-                        <label class="form-label">Fecha de Nacimiento</label>
-                        <input type="date" class="form-control">
-                    </div>
-                    <div class="col-3">
-                        <label class="form-label">Nacionalidad</label>
-                        <input type="text" class="form-control">
-                    </div>
-                </div>
-                <div class="row mb-4">
-                    <div class="col-9">
-                        <label class="form-label">Dirección</label>
-                        <input type="text" class="form-control">
-                    </div>
-                    <div class="col-3">
-                        <label class="form-label">Teléfono</label>
-                        <input type="text" class="form-control">
-                    </div>
-                </div>
-                <div class="row mb-4">
-                    <div class="col-4">
-                        <label class="form-label">Estado Civil</label>
-                        <div>
-                            <label class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="civil" checked>
-                                <span class="form-check-label">Soltero</span>
-                            </label>
-                            <label class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="civil" >
-                                <span class="form-check-label">Casado</span>
-                            </label>
-                            <label class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="civil">
-                                <span class="form-check-label">Unido</span>
-                            </label>
-                            <label class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="civil">
-                                <span class="form-check-label">Divorciado</span>
-                            </label>
-                        </div>
-                    </div>
-                    <div class="col-2">
-                        <label class="form-label">Tipo de Sangre</label>
-                            <select class="form-select" >
-                                <option selected>Seleccionar</option>
-                                <option value="1">A+</option>
-                                <option value="2">A-</option>
-                                <option value="3">B+</option>
-                                <option value="4">B-</option>
-                                <option value="5">AB+</option>
-                                <option value="5">AB-</option>
-                                <option value="5">O+</option>
-                                <option value="5">O-</option>
-                            </select>
-                    </div>
-                    <div class="col-6">
-                        <label class="form-label">Email</label>
-                        <input type="email" class="form-control">
+                <div class="col-9">
+                    <label class="form-label">Grupo Étnico</label>
+                    <div>
+                        <label class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="etnico" checked>
+                            <span class="form-check-label">Mestizo</span>
+                        </label>
+                        <label class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="etnico" >
+                            <span class="form-check-label">Maya</span>
+                        </label>
+                        <label class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="etnico" >
+                            <span class="form-check-label">Garifuna</span>
+                        </label>
+                        <label class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="etnico" >
+                            <span class="form-check-label">Xinca</span>
+                        </label>
+                        <label class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="etnico" >
+                            <span class="form-check-label">Otros / Extranjeros</span>
+                        </label>
+                        <label class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="etnico" >
+                            <span class="form-check-label">No Indica</span>
+                        </label>
                     </div>
                 </div>
-            </form>
+            </div>
+            <div class="row mb-4">
+                <div class="col-4">
+                    <label class="form-label">Profesión u Oficio</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-3">
+                    <label class="form-label">Comunidad Lingüistica</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-2">
+                    <label class="form-label">Fecha de Nacimiento</label>
+                    <input type="date" class="form-control">
+                </div>
+                <div class="col-3">
+                    <label class="form-label">Nacionalidad</label>
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+            <div class="row mb-4">
+                <div class="col-9">
+                    <label class="form-label">Dirección</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-3">
+                    <label class="form-label">Teléfono</label>
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+            <div class="row mb-4">
+                <div class="col-4">
+                    <label class="form-label">Estado Civil</label>
+                    <div>
+                        <label class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="civil" checked>
+                            <span class="form-check-label">Soltero</span>
+                        </label>
+                        <label class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="civil" >
+                            <span class="form-check-label">Casado</span>
+                        </label>
+                        <label class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="civil">
+                            <span class="form-check-label">Unido</span>
+                        </label>
+                        <label class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="civil">
+                            <span class="form-check-label">Divorciado</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <label class="form-label">Tipo de Sangre</label>
+                        <select class="form-select" >
+                            <option selected>Seleccionar</option>
+                            <option value="1">A+</option>
+                            <option value="2">A-</option>
+                            <option value="3">B+</option>
+                            <option value="4">B-</option>
+                            <option value="5">AB+</option>
+                            <option value="5">AB-</option>
+                            <option value="5">O+</option>
+                            <option value="5">O-</option>
+                        </select>
+                </div>
+                <div class="col-6">
+                    <label class="form-label">Email</label>
+                    <input type="email" class="form-control">
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- application/views/admin/donadores/examen_fisico_view.php
+++ application/views/admin/donadores/examen_fisico_view.php
@@ -4,7 +4,32 @@
             <h3 class="card-title">Examén Físico y Datos Clínicos</h3>
         </div>
         <div class="card-body">
-            <h3>;lsdf;lsdfl;</h3>
+            <div class="row mb-4">
+                <div class="col-2">
+                    <label class="form-label">Peso Lb.</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-2">
+                    <label class="form-label">Temperatura °C</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-2">
+                    <label class="form-label">Presión Arterial</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-2">
+                    <label class="form-label">Pulso</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-2">
+                    <label class="form-label">hemoglobina</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-2">
+                    <label class="form-label">Hematrocito</label>
+                    <input type="text" class="form-control">
+                </div>
+            </div>
         </div>
     </div>
 </div>
